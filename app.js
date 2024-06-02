21f0c5901824a712f21b504cd1440762580dc196
ShoTuPalush <tsehma.live@gmail.com>
2024-06-02 12:02:15 +0300

    3

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static("react-page/dist"));
+app.use(express.static(__dirname + "/react-page/dist"));
 
 app.get("/", (req, res) => {
   res.sendFile("index.html");
