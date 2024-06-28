edf7dd02e44f2f1ad199c80769d99dd7dfaa8510
ShoTuPalush <tsehma.live@gmail.com>
2024-06-28 14:18:09 +0300

    2

--- dambit.net.ua/www/react-page/src/App.jsx
+++ dambit.net.ua/www/react-page/src/App.jsx
@@ -1,38 +1,9 @@
-import { useState } from "react";
-
 import "./App.css";
 
-import Editor from "./components/editor/Editor";
-import EditorTextParser from "./components/editor-parser/EditorTextParser";
-import jsona from "./components/editor/data/exampleData.json";
-
 function App() {
-  const [isEditMode, setIsEditMode] = useState(false);
-  const [data, setData] = useState(jsona);
-
-  function toggleEditMode() {
-    if (isEditMode) {
-      setIsEditMode(false);
-      console.log("Edit mode is now disabled");
-    } else {
-      setIsEditMode(true);
-      console.log("Edit mode is now enabled");
-    }
-  }
-
   return (
     <div className="App">
-      <button id="toggle-edit-btn" onClick={toggleEditMode}>
-        Toggle Edit Mode
-      </button>
-
-      <div className="app-content">
-        {isEditMode ? (
-          <Editor data={data} setData={setData} />
-        ) : (
-          <EditorTextParser data={data} />
-        )}
-      </div>
+      <h1>pedor</h1>
     </div>
   );
 }
